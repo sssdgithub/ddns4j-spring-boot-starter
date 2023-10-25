a0f0e59b4ebd5375abe31ea8b8006017ac6169d8
SSSD <15829762162@163.com>
2023-10-25 14:03:57 +0800

    修复了如果没有配置easy-mode选项报错的情况

--- ddns4j-spring-boot-starter/src/main/java/top/sssd/ddns4j/autoconfigure/DDns4jAutoConfiguration.java
+++ ddns4j-spring-boot-starter/src/main/java/top/sssd/ddns4j/autoconfigure/DDns4jAutoConfiguration.java
@@ -3,26 +3,16 @@ package top.sssd.ddns4j.autoconfigure;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.util.CollectionUtils;
-import top.sssd.ddns.common.enums.RecordTypeEnum;
-import top.sssd.ddns.common.enums.UpdateFrequencyEnum;
-import top.sssd.ddns.model.entity.ParsingRecord;
-import top.sssd.ddns.service.IParsingRecordService;
 import top.sssd.ddns4j.autoconfigure.datasource.DDns4jH2DataSourceInitializer;
 import top.sssd.ddns4j.autoconfigure.hook.DDns4jClearDnsRecordShutDownHook;
-import top.sssd.ddns4j.autoconfigure.condition.EasyModeCondition;
+import top.sssd.ddns4j.autoconfigure.hook.DDns4jContextRefreshedEasyMode;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 
 /**
@@ -49,37 +39,9 @@ public class DDns4jAutoConfiguration  {
         return dataSourceInitializer;
     }
 
-    @Autowired
-    private IParsingRecordService parsingRecordService;
-    @Autowired
-    private DDns4jProperties dDns4jProperties;
-
-    @EventListener
-    @Conditional(EasyModeCondition.class)
-    public void handleContextRefreshedEasyMode(ContextRefreshedEvent event) throws Exception {
-        ParsingRecord parsingRecord = new ParsingRecord();
-        parsingRecord.setState(1);
-        parsingRecord.setRecordType(RecordTypeEnum.IPV6.getIndex());
-        parsingRecord.setGetIpMode(1);
-        parsingRecord.setServiceProviderId(dDns4jProperties.getEasyMode().getServiceProviderId());
-        parsingRecord.setServiceProviderSecret(dDns4jProperties.getEasyMode().getServiceProviderSecret());
-        parsingRecord.setServiceProvider(dDns4jProperties.getEasyMode().getServiceProvider());
-        parsingRecord.setUpdateFrequency(UpdateFrequencyEnum.ONE_MINUTE.getCode());
-        String domain = dDns4jProperties.getEasyMode().getDomain();
-        parsingRecord.setDomain(domain);
-
-
-        List<ParsingRecord> list = parsingRecordService.lambdaQuery()
-                .eq(ParsingRecord::getDomain, dDns4jProperties.getEasyMode().getDomain())
-                .eq(ParsingRecord::getState,1)
-                .list();
-        if (CollectionUtils.isEmpty(list)) {
-            parsingRecordService.add(parsingRecord);
-            log.info("ddns4j已正常启动,您填入的域名为:{}",domain);
-            return;
-        }
-        parsingRecordService.modify(list.get(0));
-        log.info("ddns4j已正常启动,您填入的域名为:{}",domain);
+    @Bean
+    public DDns4jContextRefreshedEasyMode handleContextRefreshedEasyMode() {
+        return new DDns4jContextRefreshedEasyMode();
     }
 
 
---
+++ ddns4j-spring-boot-starter/src/main/java/top/sssd/ddns4j/autoconfigure/hook/DDns4jContextRefreshedEasyMode.java
@@ -0,0 +1,69 @@
+package top.sssd.ddns4j.autoconfigure.hook;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import top.sssd.ddns.common.enums.RecordTypeEnum;
+import top.sssd.ddns.common.enums.UpdateFrequencyEnum;
+import top.sssd.ddns.model.entity.ParsingRecord;
+import top.sssd.ddns.service.IParsingRecordService;
+import top.sssd.ddns4j.autoconfigure.DDns4jProperties;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author sssd
+ * @careate 2023-10-25-13:35
+ */
+@Slf4j
+@Component
+public class DDns4jContextRefreshedEasyMode implements ApplicationRunner {
+
+    @Resource
+    private IParsingRecordService parsingRecordService;
+    @Resource
+    private DDns4jProperties dDns4jProperties;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if(Objects.isNull(dDns4jProperties)
+                || Boolean.FALSE.equals(dDns4jProperties.getEnabled())
+                || Objects.isNull(dDns4jProperties.getEasyMode())){
+            return;
+        }
+        ParsingRecord parsingRecord = new ParsingRecord();
+        parsingRecord.setState(1);
+        parsingRecord.setRecordType(RecordTypeEnum.IPV6.getIndex());
+        parsingRecord.setGetIpMode(1);
+        parsingRecord.setServiceProviderId(dDns4jProperties.getEasyMode().getServiceProviderId());
+        parsingRecord.setServiceProviderSecret(dDns4jProperties.getEasyMode().getServiceProviderSecret());
+        parsingRecord.setServiceProvider(dDns4jProperties.getEasyMode().getServiceProvider());
+        parsingRecord.setUpdateFrequency(UpdateFrequencyEnum.ONE_MINUTE.getCode());
+        String domain = dDns4jProperties.getEasyMode().getDomain();
+        parsingRecord.setDomain(domain);
+
+        List<ParsingRecord> list = parsingRecordService.lambdaQuery()
+                .eq(ParsingRecord::getDomain, dDns4jProperties.getEasyMode().getDomain())
+                .eq(ParsingRecord::getState,1)
+                .list();
+        if (CollectionUtils.isEmpty(list)) {
+            try {
+                parsingRecordService.add(parsingRecord);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            log.info("ddns4j已正常启动,您填入的域名为:{}",domain);
+            return;
+        }
+        try {
+            parsingRecordService.modify(list.get(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("ddns4j已正常启动,您填入的域名为:{}",domain);
+    }
+}
